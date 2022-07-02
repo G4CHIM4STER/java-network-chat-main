3cbcc24b349b3344de26bac8cd7c8dea47debad3
Leonid <veksler.leonid@yandex.ru>
2022-07-02 21:13:07 +0300

    Тест коммита через Bash

--- src/main/java/ru/geekbrains/app/RunClient.java
+++ src/main/java/ru/geekbrains/app/RunClient.java
@@ -2,7 +2,7 @@ package ru.geekbrains.app;
 
 import ru.geekbrains.client.ClientGuiController;
 
-
+//Тест
 public class RunClient {
     public static void main(String[] args) {
         ClientGuiController clientGuiController = new ClientGuiController();
