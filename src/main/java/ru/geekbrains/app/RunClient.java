bf29fa9bda7039721b28c620f32158fad7238166
Leonid <veksler.leonid@yandex.ru>
2022-07-02 21:15:13 +0300

    Тест коммита через IDEA

--- src/main/java/ru/geekbrains/app/RunClient.java
+++ src/main/java/ru/geekbrains/app/RunClient.java
@@ -3,6 +3,7 @@ package ru.geekbrains.app;
 import ru.geekbrains.client.ClientGuiController;
 
 //Тест
+//Тест коммита через Idea
 public class RunClient {
     public static void main(String[] args) {
         ClientGuiController clientGuiController = new ClientGuiController();
