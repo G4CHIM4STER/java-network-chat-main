51d23cdbc5a06aab7000f8c42a4b0338e391bd1f
Leonid <veksler.leonid@yandex.ru>
2022-07-02 21:18:08 +0300

    Тестим апдейт через GitDesktop

--- src/main/java/ru/geekbrains/app/RunClient.java
+++ src/main/java/ru/geekbrains/app/RunClient.java
@@ -4,6 +4,7 @@ import ru.geekbrains.client.ClientGuiController;
 
 //Тест
 //Тест коммита через Idea
+//Тест коммита через GitDesktop
 public class RunClient {
     public static void main(String[] args) {
         ClientGuiController clientGuiController = new ClientGuiController();
